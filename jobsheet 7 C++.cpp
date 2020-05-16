7573db9e3520f8d964ca3a5bbe18b84a06c55e03
Firda-Amelia <64563848+Firda-Amelia@users.noreply.github.com>
2020-05-16 19:47:26 +0700

    Create jobsheet 7 C++.cpp
    Buatlah program mengisinilai array denganperulangandengan index array dan bataspengulanganmenggunakan NIM
    Terakhir setiap mahasiswa

---
+++ jobsheet 7 C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main (){
+	cout<<"nama:fIRDA AMELIA"<<endl;
+	cout<<"nim:F1B019056"<<endl;
+	cout<<"kelompok:11"<<endl;
+	
+
+int nim [6];
+for (int i = 0; i<= 6; i++){
+cout<< "Masukkan nimke- " << i+1;
+cin>>nim[i];
+}
+}
