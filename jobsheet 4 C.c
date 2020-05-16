d072f7718983e29fbd77bc67d17ad4b98e2d4fec
Firda-Amelia <64563848+Firda-Amelia@users.noreply.github.com>
2020-05-16 19:17:01 +0700

    Create jobsheet 4 C.c
    Buatlah program penguranganmatriksduadimensidenganmenggunakan NIM dua orang mahasiswa.

---
+++ jobsheet 4 C.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+main (){
+int nim1 [2][2];
+int nim2 [2][2];
+int hasil [2][2];
+int i, j;
+printf ("Pengurangan NIM\n");
+for (i = 0; i< 2; i++){
+for (j = 0; j < 2; j++){
+printf ("Masukkan nilai index %d, %d = ", i, j);
+scanf ("%d", &nim6[i][j]);
+}
+}
+printf ("\n");
+for (i = 0; i< 2; i++){
+for (j = 0; j < 2; j++){
+printf ("Masukkan nilai index %d, %d = ", i, j);
+scanf ("%d", &nim1[i][j]);
+}
+}
+for (i = 0; i< 2; i++){
+for (j = 0; j < 2; j++){
+hasil [i][j]=nim6[i][j]-nim1[i][j];
+printf ("%d ", hasil[i][j]);
