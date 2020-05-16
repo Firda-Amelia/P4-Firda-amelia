5e1eb39c2dcb9a5065c40bd3cd4e4d348449cebe
Firda-Amelia <64563848+Firda-Amelia@users.noreply.github.com>
2020-05-16 18:57:36 +0700

    Create jobsheet 3 C.c
    Buatlah program penjumlahan matriks dua dime idengan menggunakan nim terakhir anda

---
+++ jobsheet 3 C.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+main (){
+int nim1 [2][2];
+int nim2 [2][2];
+int hasil [2][2];
+int i, j;
+printf ("Penjumlahan NIM\n");
+for (i = 0; i< 2; i++){
+for (j = 0; j < 2; j++){
+printf ("Masukkan nilai index %d, %d = ", i, j);
+scanf ("%d", &nim6[i][j]);
+}
+}
