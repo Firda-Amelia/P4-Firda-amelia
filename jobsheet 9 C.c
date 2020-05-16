7fc7d76e2608ef6837dec0334010912e8f1e4baf
Firda-Amelia <64563848+Firda-Amelia@users.noreply.github.com>
2020-05-16 19:30:08 +0700

    Create jobsheet 9 C.c
    Buatlah program array sebagai karakter dengan menampilkan nama setiap Mahasiswa

---
+++ jobsheet 9 C.c
@@ -0,0 +1,20 @@
+de <stdio.h>
+int main()
+{
+	
+printf ("nama : FIRDA AMELIA\n");
+printf ("nim : F1B019056\n ");
+printf ("kelompok : 11\n");
+
+int i;
+char nama[5]={'K','O','B','E'};
+char nama2[6]="KOBE";
+printf("Array per Karakter = ");
+for(i=0;i<=4;i++)
+{
+printf("%c",nama[i]);
+}
+printf("\n"); printf("Array string =%s\n\n",nama2);
+return 0;
+}
+
