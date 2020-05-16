d595f5d9ade62a764d2cdf73d1e5afd09d115539
Firda-Amelia <64563848+Firda-Amelia@users.noreply.github.com>
2020-05-16 19:25:05 +0700

    Create jobsheet 7 C.c
    Buatlah program mengisinilai array denganperulangandengan index array dan bataspengulanganmenggunakan NIM
    Terakhirsetiapmahasiswa

---
+++ jobsheet 7 C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main (){
+	printf ("nama : FIRDA AMELIA\n");
+    printf ("nim : F1B019056\n ");
+    printf ("kelompok : 11\n");
+
+int nim [6], i;
+for (i = 0; i<= 6; i++){
+printf ("Masukkan nimke-%i ", i+1);
+scanf ("%i", &nim[i]);
+}
+}
