db0ecd66e3f4b31b87d7c34f12ede02907e69376
Firda-Amelia <64563848+Firda-Amelia@users.noreply.github.com>
2020-05-16 19:51:54 +0700

    Create jobsheet 9 C++.cpp
    Buatlah program array sebagai karakter dengan menampilkan nama setiap Mahasiswa

---
+++ jobsheet 9 C++.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+int main ()
+{
+cout<<"nama:fIRDA AMELIA"<<endl;
+	cout<<"nim:F1B019056<<endl;
+	cout<<"kelompok:11"<<endl;
+	
+
+int i;
+char nama[5]={'N','B','A',};
+char nama2[6]="NBA";
+cout<<"Array per Karakter= ";
+for(i=0;i<=4;i++)
+{
+cout<<nama[i];
+}
+cout<<endl; cout<<"Array string ="<<nama2<<endl<<endl;
+return 0;
+}
